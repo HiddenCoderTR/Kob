fcb984f219f6f9cbfbb8073ca066c9a6d36bc2e8
Selim DEMİR <selimdemir63151@gmail.com>
2026-04-02 17:39:43 +0300

    Update L1.py

--- L1.py
+++ L1.py
@@ -1,5 +1,26 @@
 # This Level A1
+password = "1234"
+chrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz0123456789-_?*!'£#$&˜.,:;=+@<>|"
+chrList = list(chrStr)
+print(chrList)
 
-chrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz0123456789-_?*\/!'£#$&˜.,:;=+@<>|"
-chList = chrStr.list()
-print(ChList)
+numStr = "0123456789"
+numList = list(numList)
+unknownPassword = ['0', '0', '0', '0']
+i = 0
+while(True):
+  if(PasswordControl(unknownPassword)):
+    Break
+  i= i+1
+  ln = 0
+  while(ln<4):
+    unknownPassword[ln] += 1
+    
+
+print("Access Succesful. Current Password:" + unknownPassword) 
+
+def PasswordControl (uPassword):
+  if(string(uPassword) == password):
+    return True
+  else:
+    return False
