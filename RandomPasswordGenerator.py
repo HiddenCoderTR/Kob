14e0e4d5950361fd318ab0b5346f4e9f1a84d0bf
Selim DEMİR <selimdemir63151@gmail.com>
2026-04-05 11:21:20 +0300

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,25 +1,32 @@
 from random import randint
 from time import sleep
+
 allChrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz0123456789-_?*!'£#$&˜.,:;=+@<>|"
-allChrList = list(allChrStr)
 digitChrStr = "0123456789"
-digitChrList = list(digitChrStr)
 alphaChrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz"
-alphaChrList = list(alphaChrStr)
-passwordType = Input("Enter password type(digit, alpha, all):")
+
+# Python'da input küçük harfle yazılır
+passwordType = input("Enter password type (digit, alpha, all): ")
+passwordLength = int(input("Enter password Length: "))
+
 password = []
-passwordLength = int(Input("Enter password Length:"))
 i = 0
-while(i<4):
-  if(passwordType == "digit"):
-    password[i] = digitChrStr[randint(1, len(digitChrList))]
-  else if(passwordType == "alpha"):
-    password[i] = alphaChrStr[randint(1, len(alphaChrList))]
-  else if(passwordType == "all"):
-    password[i] = allChrStr[randint(1, len(allChrList))]
-  else:
-    print("Your input is not valid. Exiting int 10 seconds")
-    sleep(10)
-    exit()
-  print("Password Generating:",password)
-print("Password Generated:"+password)
+
+while i < passwordLength: # 4 yerine kullanıcının istediği uzunluk
+    if passwordType == "digit":
+        # randint yerine random.choice kullanmak çok daha pratiktir ama senin mantığınla gidelim:
+        password.append(digitChrStr[randint(0, len(digitChrStr) - 1)])
+    elif passwordType == "alpha":
+        password.append(alphaChrStr[randint(0, len(alphaChrStr) - 1)])
+    elif passwordType == "all":
+        password.append(allChrStr[randint(0, len(allChrStr) - 1)])
+    else:
+        print("Your input is not valid. Exiting in 10 seconds")
+        sleep(10)
+        exit()
+    
+    i += 1 # i++ yerine Python'da bu kullanılır
+    # Listeyi ekrana basarken güzel gözükmesi için "".join kullanılır
+    print("Password Generating: " + "".join(password))
+
+print("Final Password: " + "".join(password))
