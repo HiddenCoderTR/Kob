91ee77bea682f0994f9ef6960af72445d40d9f31
Selim DEMİR <selimdemir63151@gmail.com>
2026-04-05 11:02:09 +0300

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,10 +1,13 @@
 from random import randint
-chrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz0123456789-_?*!'£#$&˜.,:;=+@<>|"
-chrList = list(chrStr)
+allChrStr = "ABCDEFGHIJKLMNOQPRSTUWXYZabcdefghijklmnoqprstuwxyz0123456789-_?*!'£#$&˜.,:;=+@<>|"
+allChrList = list(allChrStr)
+digitChrStr = "0123456789"
+digitChrList = list(digitChrStr)
+passwordType = Input("Enter password type(digit, alpha, all):")
 password = []
-passwordLength = 4
+passwordLength = int(Input("Enter password Length:"))
 i = 0
 while(i<4):
-  password[i] = chrStr[randint(1, len(chrList))]
+  password[i] = allChrStr[randint(1, len(allChrList))]
   print("Password Generating:",password)
 print("Password Generated:"+password)
